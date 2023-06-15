58570f0fed9075f5ddf91cdd8ec9e05368fe0983
VitorDeFranca <francavit.r@gmail.com>
2023-06-15 14:58:05 -0300

    Commit 1 - Base da API

---
+++ Models/Todo.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Title { get; set; }
+
+        [DisplayName("Concluído")]
+        public bool Done { get; set; }
+
+        [DisplayName("Criado Em")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [DisplayName("Última atualização")]
+        public DateTime LastUptadeDate { get; set; } = DateTime.Now;
+
+        [DisplayName("Usuário")]
+        public string User { get; set; }
+    }
+}
